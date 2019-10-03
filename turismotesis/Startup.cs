50bad0af0ee0b6d0a3b02cfac1d681b2a27f7455
bernardo <villegas.bernardo27@gmail.com>
2019-10-03 00:30:06 -0300

    Agregar archivos de proyecto.

---
+++ turismotesis/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(turismotesis.Startup))]
+namespace turismotesis
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
